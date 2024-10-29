bbfd99b100ba03f9cdd0234274da025b57161476
David Rambo <davidrambo@mailfence.com>
2024-10-29 12:16:41 -0700

    clarify benefits of using app

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -2,6 +2,13 @@ export default function Index() {
   return (
     <>
       <h1>Wish Lists</h1>
+      <p>
+        Create and share wish lists with your group. When you choose an item to
+        give to someone, they won't know but everyone else will.
+      </p>
+      <p>No duplicate gifts.</p>
+      <p>No ruined surprises.</p>
+      <br />
       <p>Select a person to view their wish list.</p>
       <p>Or select "My List" to add to and edit your own.</p>
     </>
