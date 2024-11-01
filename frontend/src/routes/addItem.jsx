216508c7b35cc5046b41473a722609af5c280ecb
David Rambo <davidrambo@mailfence.com>
2024-11-01 13:01:03 -0700

    feat: apply item entry css to addItem

--- frontend/src/routes/addItem.jsx
+++ frontend/src/routes/addItem.jsx
@@ -24,44 +24,46 @@ export default function AddItem() {
 
   return (
     <>
-      <Form method="post" id="item-form">
-        <h2>Add a Gift Idea to Your Wish List</h2>
-        <p>What is it?</p>
-        <input
-          type="text"
-          name="what"
-          aria-label="What is it?"
-          className="itemText"
-        />
+      <div className="item-entry">
+        <Form method="post" id="item-form">
+          <h2>Add a Gift Idea to Your Wish List</h2>
+          <p>What is it?</p>
+          <input
+            type="text"
+            name="what"
+            aria-label="What is it?"
+            className="itemText"
+          />
 
-        <p>Want to provide a URL to the item?</p>
-        <input
-          type="text"
-          name="itemLink"
-          aria-label="Enter a URL for the item."
-          className="itemText"
-        />
+          <p>Want to provide a URL to the item?</p>
+          <input
+            type="text"
+            name="itemLink"
+            aria-label="Enter a URL for the item."
+            className="itemText"
+          />
 
-        <p>Anything else that would help the gift giver?</p>
-        <textarea
-          name="details"
-          aria-label="Add more details here."
-          className="itemTextArea"
-        />
+          <p>Anything else that would help the gift giver?</p>
+          <textarea
+            name="details"
+            aria-label="Add more details here."
+            className="itemTextArea"
+          />
 
-        <div id="form-buttons">
-          <button type="submit">Submit</button>
+          <div id="form-buttons">
+            <button type="submit">Submit</button>
 
-          <button
-            type="button"
-            onClick={() => {
-              navigate(-1);
-            }}
-          >
-            Cancel
-          </button>
-        </div>
-      </Form>
+            <button
+              type="button"
+              onClick={() => {
+                navigate(-1);
+              }}
+            >
+              Cancel
+            </button>
+          </div>
+        </Form>
+      </div>
     </>
   );
 }
