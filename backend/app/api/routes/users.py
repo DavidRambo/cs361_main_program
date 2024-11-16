b7a49f65756960a066479f617afa2e8c7286b31c
David Rambo <davidrambo@mailfence.com>
2024-11-16 13:52:33 -0800

    add imports

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -1,1 +1,17 @@
 """API Routes pertaining to Users."""
+
+import fastapi
+
+from app.models import (
+    User,
+    UserCreate,
+    UpdatePassword,
+    UserUpdateName,
+    UserPublic,
+    UsersPublic,
+    UserForSelf,
+    UserForOthers,
+)
+
+
+router = fastapi.APIRouter()
